9fdbc998447b01064c525f65f31c933257149bbb
Vivian T <vivian.tong95@gmail.com>
2023-01-13 22:28:11 -0500

    modified tools list icons. added TS and redux, removed responsive design and styled components.

--- src/components/tools/tools.component.jsx
+++ src/components/tools/tools.component.jsx
@@ -6,12 +6,13 @@ import { ReactComponent as JSIcon } from "../../assets/javascript.svg";
 import { ReactComponent as GitIcon } from "../../assets/git.svg";
 import { ReactComponent as GithubIcon } from "../../assets/github.svg";
 import { ReactComponent as SassIcon } from "../../assets/sass.svg";
-import { ReactComponent as ResDesignIcon } from "../../assets/responsivedesign.svg";
-import { ReactComponent as SEOIcon } from "../../assets/seo.svg";
-import { ReactComponent as StyledComponentsIcon } from "../../assets/styled.svg";
+// import { ReactComponent as ResDesignIcon } from "../../assets/responsivedesign.svg";
+// import { ReactComponent as SEOIcon } from "../../assets/seo.svg";
+// import { ReactComponent as StyledComponentsIcon } from "../../assets/styled.svg";
 // import { ReactComponent as PythonIcon } from "../../assets/python.svg";
-// import { ReactComponent as ReduxIcon } from "../../assets/redux.svg";
+import { ReactComponent as ReduxIcon } from "../../assets/redux.svg";
 // import { ReactComponent as FirebaseIcon } from "../../assets/firebase.svg";
+import {ReactComponent as TypeScriptIcon} from "../../assets/typescript.svg";
 
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
@@ -30,20 +31,21 @@ const toolsList = [
   { name: "Git", icon: <GitIcon width="45px" height="45px" />, key: 5 },
   { name: "GitHub", icon: <GithubIcon width="45px" height="45px" />, key: 6 },
   { name: "Sass", icon: <SassIcon width="45px" height="45px" />, key: 7 },
-  {
-    name: "Responsive Design",
-    icon: <ResDesignIcon width="45px" height="45px" />,
-    key: 8,
-  },
-  { name: "SEO", icon: <SEOIcon width="45px" height="45px" />, key: 9 },
-  {
-    name: "Styled Components",
-    icon: <StyledComponentsIcon width="45px" height="45px" />,
-    key: 10,
-  },
+  // {
+  //   name: "Responsive Design",
+  //   icon: <ResDesignIcon width="45px" height="45px" />,
+  //   key: 8,
+  // },
+  // { name: "SEO", icon: <SEOIcon width="45px" height="45px" />, key: 9 },
+  // {
+  //   name: "Styled Components",
+  //   icon: <StyledComponentsIcon width="45px" height="45px" />,
+  //   key: 10,
+  // },
   //   { name: "Python", icon: <PythonIcon width="45px" height="45px"/>, key: 11 },
-  //   { name: "Redux", icon: <ReduxIcon width="45px" height="45px"/>, key: 12 },
+    { name: "Redux", icon: <ReduxIcon width="45px" height="45px"/>, key: 12 },
   //   { name: "Firebase", icon: <FirebaseIcon width="45px" height="45px"/>, key: 13 },
+  {name: "TypeScript", icon: <TypeScriptIcon width="45px" height="45px"/>, key:14}
 ];
 
 function Tech() {
